910add8b541b48788952c82fecc7924fd686755a
GuilhermesfAlves <guilhermesfalves17@gmail.com>
2023-11-24 20:08:23 -0300

    funfando tudo

--- vector_test.c
+++ vector_test.c
@@ -1,39 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void insere_Ordenado(int valor, int *vetor, int *tamanho) {
+void insere_Ordenado(int valor, int **vetor, int *tamanho) {
     int i, j;
 
     // Encontrar a posição correta para inserir o valor no vetor ordenado
-    for (i = 0; i < *tamanho && valor > vetor[i]; i++) {
+    for (i = 0; i < *tamanho && valor > (*vetor)[i]; i++) {
     }
 
     // Realocar o vetor se necessário
     *tamanho += 1;
-    vetor = (int *)realloc(vetor, *tamanho * sizeof(int));
+    *vetor = (int *)realloc(*vetor, *tamanho * sizeof(int));
 
     for (j = *tamanho - 1; j > i; j--) 
-        vetor[j] = vetor[j - 1];
+        (*vetor)[j] = (*vetor)[j - 1];
 
-    vetor[i] = valor;
+    (*vetor)[i] = valor;
 }
 
-void remove_Elemento(int valor, int *vetor, int *tamanho) {
+void remove_Elemento(int valor, int **vetor, int *tamanho) {
     int i, j;
 
     // Encontrar a posição do valor no vetor
-    for (i = 0; i < *tamanho && valor != vetor[i]; i++) {
+    for (i = 0; i < *tamanho && valor != (*vetor)[i]; i++) {
     }
 
     if (i == *tamanho) 
         return;
     
     for (j = i; j < *tamanho - 1; j++) {
-        vetor[j] = vetor[j + 1];
+        (*vetor)[j] = (*vetor)[j + 1];
     }
 
     *tamanho -= 1;
-    vetor = (int *)realloc(vetor, *tamanho * sizeof(int));
+    *vetor = (int *)realloc(*vetor, *tamanho * sizeof(int));
 }
 
 int main() {
@@ -42,12 +42,11 @@ int main() {
     int valor;
     char mode;
 
-
     while (scanf("%c %d", &mode, &valor) != -1) {
         if (mode == 'i')
-            insere_Ordenado(valor, vetor, &tamanho);
+            insere_Ordenado(valor, &vetor, &tamanho);
         if (mode == 'r')
-            remove_Elemento(valor, vetor, &tamanho);
+            remove_Elemento(valor, &vetor, &tamanho);
     }
 
     free(vetor);
