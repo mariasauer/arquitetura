ed61af4e9b98242993bfbd773f46b86c85b9384e
Maria Sauer <mcs22@inf.ufpr.br>
2023-11-24 11:55:56 -0300

    ahhhh
    Signed-off-by: Maria Sauer <mcs22@inf.ufpr.br>

--- matrizTres.c
+++ matrizTres.c
@@ -1,21 +1,30 @@
-//percorre matriz normal
+// percorre primeiro colunas da matriz
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
 // Função para preencher uma matriz com números aleatórios
-void preencherMatriz(float **matriz, int linhas, int colunas) {
-    for (int i = 0; i < linhas; i++) {
-        for (int j = 0; j < colunas; j++) {
-            matriz[i][j] = rand() % 10; 
+void preencherMatriz(float **matriz, int linhas, int colunas)
+{
+
+    for (int j = 0; j < colunas; j++)
+    {
+        for (int i = 0; i < linhas; i++)
+        {
+
+            matriz[i][j] = rand() % 10;
         }
     }
 }
 
 // Função para imprimir uma matriz
-void imprimirMatriz(float **matriz, int linhas, int colunas) {
-    for (int i = 0; i < linhas; i++) {
-        for (int j = 0; j < colunas; j++) {
+void imprimirMatriz(float **matriz, int linhas, int colunas)
+{
+    for (int j = 0; j < colunas; j++)
+    {
+        for (int i = 0; i < linhas; i++)
+        {
+
             printf("%0.0f\t", matriz[i][j]);
         }
         printf("\n");
@@ -23,87 +32,109 @@ void imprimirMatriz(float **matriz, int linhas, int colunas) {
 }
 
 // Função para alocar memória para uma matriz
-float** alocarMatriz(int linhas, int colunas) {
+float **alocarMatriz(int linhas, int colunas)
+{
     float **matriz = (float **)malloc(linhas * sizeof(float *));
-    for (int i = 0; i < linhas; i++) {
+    for (int i = 0; i < linhas; i++)
+    {
         matriz[i] = (float *)malloc(colunas * sizeof(float));
     }
     return matriz;
 }
 
-
 // Função para somar duas matrizes
-void somarMatrizes(float **matriz1, float **matriz2, float **resultado, int linhas, int colunas) {
-    for (int i = 0; i < linhas; i++) {
-        for (int j = 0; j < colunas; j++) {
+void somarMatrizes(float **matriz1, float **matriz2, float **resultado, int linhas, int colunas)
+{
+    for (int j = 0; j < colunas; j++)
+    {
+        for (int i = 0; i < linhas; i++)
+        {
+
             resultado[i][j] = matriz1[i][j] + matriz2[i][j];
         }
     }
 }
 
 // Função para calcular a matriz transposta
-void calcularTransposta(float **matriz, float **transposta, int linhas, int colunas) {
-    for (int i = 0; i < linhas; i++) {
-        for (int j = 0; j < colunas; j++) {
+void calcularTransposta(float **matriz, float **transposta, int linhas, int colunas)
+{
+    for (int j = 0; j < colunas; j++)
+    {
+        for (int i = 0; i < linhas; i++)
+        {
+
             transposta[j][i] = matriz[i][j];
         }
     }
-
 }
 
-
-
 // Função para liberar memória de uma matriz
-void liberarMatriz(float **matriz, int linhas) {
-    for (int i = 0; i < linhas; i++) {
+void liberarMatriz(float **matriz, int linhas)
+{
+    for (int i = 0; i < linhas; i++)
+    {
         free(matriz[i]);
     }
     free(matriz);
 }
 
 // Função para calcular a matriz inversa (para matrizes 3x3)
-int calcularMatrizInversa(float **matriz, float **inversa, int linhas) {
+int calcularMatrizInversa(float **matriz, float **inversa, int linhas)
+{
     // Implementação simplificada usando eliminação de Gauss-Jordan
     // Esta implementação assume que a matriz é invertível
 
     // Copia a matriz original para a matriz inversa
-    for (int i = 0; i < linhas; i++) {
-        for (int j = 0; j < linhas; j++) {
+    for (int j = 0; j < linhas; j++)
+    {
+        for (int i = 0; i < linhas; i++)
+        {
+
             inversa[i][j] = matriz[i][j];
         }
     }
 
     // Inicializa a matriz de identidade
 
-  float **identidade = alocarMatriz(linhas, linhas);
+    float **identidade = alocarMatriz(linhas, linhas);
 
     // Inicializando a matriz identidade
-    for (int i = 0; i <linhas; i++) {
-        for (int j = 0; j <linhas; j++) {
-            if (i == j) {
+    for (int j = 0; j < linhas; j++)
+    {
+        for (int i = 0; i < linhas; i++)
+        {
+
+            if (i == j)
+            {
                 identidade[i][j] = 1;
-            } else {
+            }
+            else
+            {
                 identidade[i][j] = 0;
             }
         }
     }
 
-
     // Aplica a eliminação de Gauss-Jordan
-    for (int k = 0; k <linhas; k++) {
+    for (int k = 0; k < linhas; k++)
+    {
         float pivo = inversa[k][k];
 
         // Divide a linha pelo pivô
-        for (int j = 0; j <linhas; j++) {
+        for (int j = 0; j < linhas; j++)
+        {
             inversa[k][j] /= pivo;
             identidade[k][j] /= pivo;
         }
 
         // Reduz outras linhas
-        for (int i = 0; i <linhas; i++) {
-            if (i != k) {
+        for (int i = 0; i < linhas; i++)
+        {
+            if (i != k)
+            {
                 float fator = inversa[i][k];
-                for (int j = 0; j <linhas; j++) {
+                for (int j = 0; j < linhas; j++)
+                {
                     inversa[i][j] -= fator * inversa[k][j];
                     identidade[i][j] -= fator * identidade[k][j];
                 }
@@ -112,8 +143,11 @@ int calcularMatrizInversa(float **matriz, float **inversa, int linhas) {
     }
 
     // A matriz identidade agora contém a inversa
-    for (int i = 0; i <linhas; i++) {
-        for (int j = 0; j <linhas; j++) {
+    for (int j = 0; j < linhas; j++)
+    {
+        for (int i = 0; i < linhas; i++)
+        {
+
             inversa[i][j] = identidade[i][j];
         }
     }
@@ -124,30 +158,34 @@ int calcularMatrizInversa(float **matriz, float **inversa, int linhas) {
 }
 
 // Função para multiplicar duas matrizes
-void multiplicarMatrizes(float **matriz1, float **matriz2, float **resultado, int linhas1, int colunas1) {
+void multiplicarMatrizes(float **matriz1, float **matriz2, float **resultado, int linhas1, int colunas1)
+{
     int colunas2 = colunas1;
-    for (int i = 0; i < linhas1; i++) {
-        for (int j = 0; j < colunas2; j++) {
+    for (int j = 0; j < colunas2; j++)
+    {
+        for (int i = 0; i < linhas1; i++)
+        {
+
             resultado[i][j] = 0.0;
-            for (int k = 0; k < colunas1; k++) {
+            for (int k = 0; k < colunas1; k++)
+            {
                 resultado[i][j] += matriz1[i][k] * matriz2[k][j];
             }
         }
     }
 }
 
-
-int main() {
+int main()
+{
     // Inicializa o gerador de números aleatórios com base no tempo atual
     srand(time(NULL));
 
     // Define o tamanho das matrizes
-    int linhas = 1024;
-    int colunas = 1024;
+    int linhas = 2048;
+    int colunas = 2048;
 
     float **matriz1 = alocarMatriz(linhas, colunas);
     float **matriz2 = alocarMatriz(linhas, colunas);
-
 
     // Preenche as matrizes com números aleatórios
     preencherMatriz(matriz1, linhas, colunas);
@@ -177,16 +215,18 @@ int main() {
     imprimirMatriz(transposta, linhas, colunas);
     liberarMatriz(transposta, linhas);
 
-
     // Cria uma matriz para armazenar a inversa
     float **inversa = alocarMatriz(linhas, colunas);
 
     // Chama a função para calcular a matriz inversa
-    if (calcularMatrizInversa(matriz1, inversa, linhas)) {
+    if (calcularMatrizInversa(matriz1, inversa, linhas))
+    {
         // Imprime a matriz inversa
         printf("\nMatriz Inversa:\n");
         imprimirMatriz(inversa, linhas, colunas);
-    } else {
+    }
+    else
+    {
         printf("\nA matriz não é invertível.\n");
     }
 
@@ -201,6 +241,6 @@ int main() {
     liberarMatriz(matriz1, linhas);
     liberarMatriz(matriz2, linhas);
     liberarMatriz(resultado, linhas);
-    
+
     return 0;
 }
